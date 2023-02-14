f0f3714b3b2530d23aac021adb63828a22a0adf2
shinas_v_b <myofficial.shinas@gmail.com>
2023-02-14 15:03:19 +0530

    course_err

--- config/allowedOrgin.js
+++ config/allowedOrgin.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5000",
   "http://127.0.0.1:5173",
   "https://social-app-sm6d.onrender.com",
+  "https://social-app-sm6d.onrender.com/post",
   "https://social-app-frontend-vej8.onrender.com",
 ];
 
