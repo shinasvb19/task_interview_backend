043295ab028753b6d46e56e27066958b226a38fb
shinas_v_b <myofficial.shinas@gmail.com>
2023-02-14 14:54:07 +0530

    url_change

--- config/allowedOrgin.js
+++ config/allowedOrgin.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
   "http://127.0.0.1:5000",
   "http://127.0.0.1:5173",
-  "http://localhost:5000",
-  "http://localhost:5173",
+  "https://social-app-sm6d.onrender.com",
+  "https://social-app-frontend-vej8.onrender.com",
 ];
 
 module.exports = allowedOrigins;
