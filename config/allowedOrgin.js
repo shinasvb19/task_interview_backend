89773e80e7ebc615981b334a68836ff5d9e5b514
shinas_v_b <myofficial.shinas@gmail.com>
2023-02-14 15:21:24 +0530

    small_fix

--- config/allowedOrgin.js
+++ config/allowedOrgin.js
@@ -1,8 +1,5 @@
 const allowedOrigins = [
-  "http://127.0.0.1:5000",
-  "http://127.0.0.1:5173",
   "https://social-app-sm6d.onrender.com",
-  "https://social-app-sm6d.onrender.com/post",
   "https://social-app-frontend-vej8.onrender.com",
 ];
 
